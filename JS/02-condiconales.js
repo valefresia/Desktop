903377bcdad4dbf5b8bf68122e96f29cf1d758b0
valefresia <131785137+valefresia@users.noreply.github.com>
2025-01-22 17:24:14 -0300

    agregue un else if y cambiamos la varibale por const

--- JS/02-condiconales.js
+++ JS/02-condiconales.js
@@ -2,20 +2,18 @@
 if (codicion logica){
     todo el codigo que quiero realizar si la condicion es verdadera
 }
-*/ 
+*/
 
 //Solicite la edad al usuario e indique si debe votar
-//16 y menores de 18 anos es optativo,mayores de 70 
-//18 a 70 
+//16 y menores de 18 anos es optativo,mayores de 70
+//18 a 70
 
-let edad = parseInt(prompt('ingrese su edad'));
+const edad = parseInt(prompt("ingrese su edad"));
 
-if (edad >=18 && edad < 70){
-    document.write('debe votar');
-}else{
-    if((edad>=16 && edad<18) || edad >70 ){
-        document.write('es optativo votar');
-    }else{
-        document.write('no debe votar');
-    }
+if (edad >= 18 && edad < 70) {
+  document.write("debe votar");
+} else if ((edad >= 16 && edad < 18) || edad > 70) {
+  document.write("es optativo votar");
+} else {
+  document.write("no debe votar");
 }
