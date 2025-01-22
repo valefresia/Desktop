6b90cbcb3fbafdbbaa5547457429c331df9072d8
valefresia <131785137+valefresia@users.noreply.github.com>
2025-01-22 09:55:18 -0300

    agregue un ejemplo de condicional doble

---
+++ JS/02-condiconales.js
@@ -0,0 +1,17 @@
+/*
+if (codicion logica){
+    todo el codigo que quiero realizar si la condicion es verdadera
+}
+*/ 
+
+//Solicite la edad al usuario e indique si debe votar
+//16 y menores de 18 anos es optativo,mayores de 70 
+//18 a 70 
+
+let edad = parseInt(prompt('ingrese su edad'));
+
+if (edad >=18){
+    document.write('debe votar');
+}else{
+    document.write('no debe votar');
+}
