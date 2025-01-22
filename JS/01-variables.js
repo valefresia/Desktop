8cbedc6cda5ad412f307e03062d19bcf3f3b7993
valefresia <131785137+valefresia@users.noreply.github.com>
2025-01-22 09:11:39 -0300

    agregamos un mensaje con document write y un alert

---
+++ JS/01-variables.js
@@ -0,0 +1,28 @@
+//esto es un coment de una linea 
+
+/*declarar una variable que es una variable
+Es un espacio de memoria que guarda un dato 
+*/
+
+//declarar una variable LET-CONST 
+
+//LET variable que cambia el valor en el tiempo 
+//CONST variable que jamas va a cambiar 
+
+let anioActual =2024;
+const intagram = "https://www.instagram.com/";
+
+//mostrar mensajes
+console.log(anioActual);
+console.log('anioActual');
+console.log('Ano actual: '+ anioActual);
+console.log(intagram);
+
+//modificar contenido de una variable
+anioActual = 2025;
+console.log(anioActual);
+
+document.write('<p class="parrafoDestacado">Esto es una prueba de document.write</p>');
+
+//ventanas emergentes
+alert('Hola Mundo')
